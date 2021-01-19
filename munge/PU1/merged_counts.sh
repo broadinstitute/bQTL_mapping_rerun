af2a7d9944e64dba297dd4cde10fa822a808a5c1
Susie Song <susiesong@gmail.com>
2021-01-19 23:51:19 +0000

    Merge counts bash script

---
+++ munge/PU1/merged_counts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Parameters
+INPUT_PATH="processed/PU1/merged_counts"
+TEMP_FILE="results/PU1/TEMP.txt"
+OUTPUT_TEMP_FILE="results/PU1/TEMP_OUTPUT.txt"
+OUTPUT_FILE="results/PU1/counts.merged.txt"
+
+rm $OUTPUT_FILE
+
+first=true
+for f in ${INPUT_PATH}/*.txt; do
+    if [ $first == true ]
+    then
+        # Copy the geneid, chr, start, end, strand, length, and counts columns from first file
+        tail -n +2 $f > $OUTPUT_TEMP_FILE 
+        first=false
+    else 
+        # Copy only the last column (counts) from all other files
+        awk '{print $NF}' $f | tail -n +2 > $TEMP_FILE
+        paste $OUTPUT_TEMP_FILE $TEMP_FILE > $OUTPUT_FILE
+        cp -f $OUTPUT_FILE $OUTPUT_TEMP_FILE
+    fi
+done
+
+rm $TEMP_FILE
+rm $OUTPUT_TEMP_FILE
